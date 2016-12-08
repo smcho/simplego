726e3e3662f14af85b97a6a65014aeeadf5f7bb8
Sangmin Cho <celdi77@gmail.com>
2016-12-08 21:35:59 +0900

    logging; no meaning

--- gopjt/cms/cmd/main.go
+++ gopjt/cms/cmd/main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"os"
+	"log"
 
 	"github.com/smcho/simplego/gopjt/cms"
 )
 
+func init() {
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.Lmicroseconds)
+}
+
 func main() {
+	for _, tmpl := range cms.Tmpl.Templates() {
+		log.Printf("%v", *tmpl)
+	}
+
 	p := &cms.Page{
 		Title:   "Hello, world!",
 		Content: "This is the body of our webpage",
