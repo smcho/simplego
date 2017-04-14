38350f21b08b45442b94575a2b7eac4d7571b1c2
Sangmin Cho <celdi77@gmail.com>
2017-04-14 11:25:38 +0900

    how to get mongo ObjectId of the new document

--- singles/mongo/mgo_simiple.go
+++ singles/mongo/mgo_simiple.go
@@ -12,6 +12,7 @@ import (
 )
 
 type Person struct {
+	Id bson.ObjectId `bson:"_id"`
 	Name string
 	Phone string
 }
@@ -69,17 +70,42 @@ func main() {
 
 	c := session.DB("").C("people")
 
-	//iErr := c.Insert(&Person{"Ale", "+55 53 8116 9639"},
-	//	&Person{"Cla", "+55 53 8402 8510"})
-	//if iErr != nil {
-	//	log.Fatal(iErr)
-	//}
+	if false {
+		iErr := c.Insert(&Person{ bson.NewObjectId(),"Ale", "+55 53 8116 9639"},
+			&Person{bson.NewObjectId(), "Cla", "+55 53 8402 8510"})
+		if iErr != nil {
+			log.Fatal(iErr)
+		}
+	}
+	if false {
+		id1, iErr1 := persistNewPerson("조상민", "010 2345 6789", c)
+		if iErr1 != nil {
+			log.Fatal(iErr1)
+		}
+		log.Printf("new person: id=%s", id1)
+	}
+
 
 	result := Person{}
-	fErr := c.Find(bson.M{"name": "Ale"}).One(&result)
+	fErr := c.Find(bson.M{"name": "조상민"}).One(&result)
 	if fErr != nil {
 		log.Fatal(fErr)
 	}
 
-	log.Printf("found: %v", result.Phone)
+	log.Printf("found: %v", result)
+}
+
+func persistNewPerson(name, phone string, c *mgo.Collection) (string, error) {
+	p := Person {
+		Id: bson.NewObjectId(),
+		Name: name,
+		Phone: phone,
+	}
+
+	err := c.Insert(&p)
+	if err != nil {
+		return  "", err
+	}
+
+	return p.Id.Hex(), nil
 }
